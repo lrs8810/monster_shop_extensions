ffaaa13f618c8bd5b557fdde1e9d447085580f4d
Laura Schulz <lrs8810@gmail.com>
2019-11-04 22:22:35 -0700

    Update db and schema

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,89 +10,100 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_191_028_033_459) do
+ActiveRecord::Schema.define(version: 20191105052050) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'item_orders', force: :cascade do |t|
-    t.bigint 'order_id'
-    t.bigint 'item_id'
-    t.float 'price'
-    t.integer 'quantity'
-    t.integer 'status', default: 0
-    t.bigint 'merchant_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['item_id'], name: 'index_item_orders_on_item_id'
-    t.index ['merchant_id'], name: 'index_item_orders_on_merchant_id'
-    t.index ['order_id'], name: 'index_item_orders_on_order_id'
+  create_table "addresses", force: :cascade do |t|
+    t.string "addresses"
+    t.string "city"
+    t.string "state"
+    t.string "zip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
-  create_table 'items', force: :cascade do |t|
-    t.string 'name'
-    t.string 'description'
-    t.float 'price'
-    t.string 'image'
-    t.boolean 'active?', default: true
-    t.integer 'inventory'
-    t.bigint 'merchant_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.boolean 'enabled?', default: true
-    t.index ['merchant_id'], name: 'index_items_on_merchant_id'
+  create_table "item_orders", force: :cascade do |t|
+    t.bigint "order_id"
+    t.bigint "item_id"
+    t.float "price"
+    t.integer "quantity"
+    t.integer "status", default: 0
+    t.bigint "merchant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_item_orders_on_item_id"
+    t.index ["merchant_id"], name: "index_item_orders_on_merchant_id"
+    t.index ["order_id"], name: "index_item_orders_on_order_id"
   end
 
-  create_table 'merchants', force: :cascade do |t|
-    t.string 'name'
-    t.string 'address'
-    t.string 'city'
-    t.string 'state'
-    t.integer 'zip'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.float "price"
+    t.string "image"
+    t.boolean "active?", default: true
+    t.integer "inventory"
+    t.bigint "merchant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "enabled?", default: true
+    t.index ["merchant_id"], name: "index_items_on_merchant_id"
   end
 
-  create_table 'orders', force: :cascade do |t|
-    t.string 'name'
-    t.string 'address'
-    t.string 'city'
-    t.string 'state'
-    t.integer 'zip'
-    t.integer 'status', default: 0
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.bigint 'user_id'
-    t.index ['user_id'], name: 'index_orders_on_user_id'
+  create_table "merchants", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "city"
+    t.string "state"
+    t.integer "zip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "enabled?", default: true
   end
 
-  create_table 'reviews', force: :cascade do |t|
-    t.string 'title'
-    t.string 'content'
-    t.integer 'rating'
-    t.bigint 'item_id'
-    t.index ['item_id'], name: 'index_reviews_on_item_id'
+  create_table "orders", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "city"
+    t.string "state"
+    t.integer "zip"
+    t.integer "status", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'name'
-    t.string 'address'
-    t.string 'city'
-    t.string 'state'
-    t.string 'zip'
-    t.string 'email'
-    t.string 'password_digest'
-    t.integer 'role', default: 0
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.bigint 'merchant_id'
-    t.index ['merchant_id'], name: 'index_users_on_merchant_id'
+  create_table "reviews", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
+    t.integer "rating"
+    t.bigint "item_id"
+    t.index ["item_id"], name: "index_reviews_on_item_id"
   end
 
-  add_foreign_key 'item_orders', 'items'
-  add_foreign_key 'item_orders', 'merchants'
-  add_foreign_key 'item_orders', 'orders'
-  add_foreign_key 'items', 'merchants'
-  add_foreign_key 'orders', 'users'
-  add_foreign_key 'reviews', 'items'
-  add_foreign_key 'users', 'merchants'
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.integer "role", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "merchant_id"
+    t.boolean "enabled?", default: true
+    t.index ["merchant_id"], name: "index_users_on_merchant_id"
+  end
+
+  add_foreign_key "addresses", "users"
+  add_foreign_key "item_orders", "items"
+  add_foreign_key "item_orders", "merchants"
+  add_foreign_key "item_orders", "orders"
+  add_foreign_key "items", "merchants"
+  add_foreign_key "orders", "users"
+  add_foreign_key "reviews", "items"
+  add_foreign_key "users", "merchants"
 end
