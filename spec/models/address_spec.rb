7f63f73039078b9a4a0ab4df240f7e85bd48cfb8
Laura Schulz <lrs8810@gmail.com>
2019-11-06 00:45:11 -0700

    Add instance method test for address model

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -28,4 +28,25 @@ describe Address, type: :model do
       expect(home_address.nickname).to eq('Home')
     end
   end
+
+  describe 'instance methods' do
+    it 'shipped_orders?' do
+      @user = User.create(
+        name: 'Bob',
+        email: 'bob@email.com',
+        password: 'secure'
+      )
+      @address_1 = @user.addresses.create(address: '123', city: 'SA', state: 'TX', zip: 80_201)
+      @address_2 = @user.addresses.create(address: '124 Main', city: 'SA', state: 'TX', zip: 78240, nickname: 2)
+      order_1 = @user.orders.create(name: 'User 1', address_id: @address_1.id, status: 2)
+      order_2 = @user.orders.create(name: 'User 1', address_id: @address_1.id, status: 0)
+      order_3 = @user.orders.create(name: 'User 1', address_id: @address_2.id, status: 1)
+
+      expect(@address_1.orders.count).to eq(2)
+      expect(@address_1.shipped_orders?).to eq(true)
+
+      expect(@address_2.orders.count).to eq(1)
+      expect(@address_2.shipped_orders?).to eq(false)
+    end
+  end
 end
